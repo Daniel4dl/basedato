567e9f83a48959b44aab5740fa65910403e97765
daniel <daniel4dl17@gmail.com>
2019-01-21 11:27:45 -0700

    jose nose XD

--- leerNombre/src/leernombre/LeerNombre.java
+++ leerNombre/src/leernombre/LeerNombre.java
@@ -11,49 +11,77 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  *
  * @author dii
  */
 public class LeerNombre {
-
-    /**
-     * @param args the command line arguments
-     */
+   static int palabras = 0;
     static void leerArchivo(String archivo) throws FileNotFoundException, IOException {
         File arc = new File(archivo);
         String cadena;
-        
+        ArrayList total;
         String cadenas[];
-        String cadenas2[];
-         String cadenas3[];
-        int palabras = 0;
+
+
         int c = 0;
         FileReader f = new FileReader(archivo);
         BufferedReader b = new BufferedReader(f);
         while ((cadena = b.readLine()) != null) {
-            c++;  
-            cadenas = cadena.split(" ");
-            for (String i:cadenas) {
-                System.out.println(i);
-                 cadenas2=i.split("./");
-                 
-                                  cadenas3=i.split("-");
+            c++;
 
-                 
-                palabras+=cadenas2.length+cadenas3.length;
+            if (cadena.contains(" ")){
+
+                cadenas=cadena.split(" ");
+                total=new ArrayList();
+                for (String i:cadenas
+                     ) {
+                    if (i.length()>0){
+                        Pattern p = Pattern.compile("[a-zA-Z]*");
+                        Matcher m = p.matcher(i);
+                        Pattern l = Pattern.compile("[0-9.0-9]*");
+                        Matcher v = l.matcher(i);
+                        if (m.matches()|v.matches()){
+                            total.add(i);
+                        }else {
+                            Palabras(i);
+                        }
+                    }
+
+                }
+                palabras+=total.size();
             }
-          
             
-            
-            if(cadenas.length>1){
-            palabras += cadenas.length;
-            }
+
+
         }
 
         System.out.println(arc.getName() + " " + palabras + " " + c);
         b.close();
+
+    }
+    static void Palabras(String i){
+        if (i.contains("-")){
+           String cadena;
+           String cadenas[];
+           ArrayList totol=new ArrayList();
+           cadenas=i.split("-");
+            for (String j:cadenas
+                 ) {
+                Pattern p = Pattern.compile("[^/]*");
+                Matcher m = p.matcher(j);
+                if (m.matches()){
+            totol.add(j);
+                }
+                else{
+                    System.out.println(j);
+                }
+            }
+            palabras+=totol.size();
+        }
 
     }
 
