a7d5bc64f57dfbbe08aa1bf61fa65d95fafb15df
Martinez Guzman Luis Daniel <daniel4dl17@gmail.com>
2019-01-22 15:40:47 -0800

    Add files via upload

--- leerNombre/src/leernombre/LeerNombre.java
+++ leerNombre/src/leernombre/LeerNombre.java
@@ -5,7 +5,11 @@
  */
 package leernombre;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -15,33 +19,11 @@ import java.util.regex.Pattern;
  * @author dii
  */
 public class LeerNombre {
-   static int palabras = 0;
-    static void leerArchivo(String archivo) throws FileNotFoundException, IOException {
-        File arc = new File(archivo);
-        String cadena;
-        ArrayList total;
-        String cadenas[];
-
-
-        int c = 0;
-        FileReader f = new FileReader(archivo);
-        BufferedReader b = new BufferedReader(f);
-        while ((cadena = b.readLine()) != null) {
-            c++;
-            StringTokenizer st = new StringTokenizer(cadena);
-           palabras+=st.countTokens();
-
-        }
-
-        System.out.println(arc.getName() + " " + palabras + " " + c);
-        b.close();
-
-    }
 
     static void leerDirectorio(String directorio) throws IOException {
         Queue<String> archivo = new LinkedList<>();
         boolean file = true;
-        int a=0;
+        int a = 0;
 
         Stack<String> path = new Stack<>();
         File f = new File(directorio);
@@ -60,10 +42,31 @@ public class LeerNombre {
 
             }
         }
+   
     }
-    public static void main(String[] args) throws IOException {
 
-        leerArchivo(args[0]);
+    static void leerArchivo(String archivo) throws FileNotFoundException, IOException {
+        File arc = new File(archivo);
+        String cadena;
+        int palabras = 0;
+
+        int c = 0;
+        FileReader f = new FileReader(archivo);
+        BufferedReader b = new BufferedReader(f);
+        while ((cadena = b.readLine()) != null) {
+            c++;
+            StringTokenizer st = new StringTokenizer(cadena);
+            palabras += st.countTokens();
+
+        }
+
+        System.out.println(c + " " + palabras + " " + arc.getName());
+        b.close();
+    }
+
+    public static void main(String[] args) throws IOException {
+        leerDirectorio(args[0]);
+
     }
 
 }
